5d0f236248b601f0b4c9a9b7561a7bf013e51f10
deathnote2501 <107842385+deathnote2501@users.noreply.github.com>
2024-07-24 15:52:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def main():
     
     if password == "jeromeIA":  # Replace with the desired password
         # Create a file uploader for multiple PDF files
-        uploaded_files = st.file_uploader("Chargez vos fichiers PDF à concaténer", type="pdf", accept_multiple_files=True)
+        uploaded_files = st.file_uploader("Chargez vos fichiers PDF à concaténer ci-dessous :", type="pdf", accept_multiple_files=True)
         
         if uploaded_files:
             st.write(f"Number of files uploaded: {len(uploaded_files)}")
