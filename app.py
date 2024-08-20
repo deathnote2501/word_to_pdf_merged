565b70a9115f2b028eefaee8178515cfe3c8788a
deathnote2501 <107842385+deathnote2501@users.noreply.github.com>
2024-08-20 17:04:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,35 +9,29 @@ def main():
     # Password input
     password = st.text_input("Entrez le mot de passe pour accéder à l'application", type="password")
     
-    if password == "jeromeIA":  # Mot de passe désiré
-        # Create a file uploader for multiple PDF files
+    if password == "jeromeIA":  # Mot de passe correct
         st.write("")
         st.write("")
         st.write("")
         st.write("")
         st.write("")
         st.subheader("Chargez vos fichiers PDF ci-dessous :")
+        
+        # Create a file uploader for multiple PDF files
         uploaded_files = st.file_uploader("", type="pdf", accept_multiple_files=True)
 
-    else:
-        if password:  # Vérifie que l'utilisateur a saisi quelque chose
-            st.warning("Mot de passe incorrect.")
-        
         if uploaded_files:
             st.write(f"Nombre de fichiers chargés : {len(uploaded_files)}")
-            
+
             # Crée un objet PdfWriter pour fusionner les fichiers
             pdf_writer = PdfWriter()
 
             # Parcourt les fichiers PDF chargés
             try:
                 for uploaded_file in uploaded_files:
-                    try:
-                        pdf_reader = PdfReader(uploaded_file)
-                        for page_num in range(len(pdf_reader.pages)):
-                            pdf_writer.add_page(pdf_reader.pages[page_num])
-                    except Exception as e:
-                        st.error(f"Erreur lors de la lecture du fichier {uploaded_file.name} : {str(e)}")
+                    pdf_reader = PdfReader(uploaded_file)
+                    for page_num in range(len(pdf_reader.pages)):
+                        pdf_writer.add_page(pdf_reader.pages[page_num])
 
                 # Enregistre le PDF fusionné dans un objet BytesIO
                 merged_pdf = BytesIO()
@@ -56,13 +50,13 @@ def main():
                 )
             except Exception as e:
                 st.error(f"Erreur lors de la fusion des fichiers PDF : {str(e)}")
-        
+
         st.write("")
         st.write("")
         st.write("")
         st.write("© 2024 Jérome Iavarone - jerome.iavarone@gmail.com")
-        
-    else:
+
+    elif password:  # Vérifie que l'utilisateur a saisi un mot de passe incorrect
         st.warning("Mot de passe incorrect.")
 
 if __name__ == "__main__":
